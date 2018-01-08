f6899ae1c022fbd0c83865ef2dfb96b92982f95f
Gururaj R Pandurangi <gpandurang001@nam.ad.pwcinternal.com>
2018-01-08 23:39:28 +0530

    initial commit

---
+++ player/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from gameplay.models import Game
+
+def home(request):
+    return render(request, "player/home.html",
+                  {'ngames': Game.objects.count()})
+
