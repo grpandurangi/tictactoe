77815098ca90f315db61dbce6623c3fec202455a
Gururaj R Pandurangi <gpandurang001@nam.ad.pwcinternal.com>
2018-01-09 16:50:44 +0530

    Updated

--- player/views.py
+++ player/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-
 from gameplay.models import Game
 
+
 def home(request):
+    my_games = Game.objects.games_for_user(request.user)
+    active_games = my_games.active()
+
     return render(request, "player/home.html",
-                  {'ngames': Game.objects.count()})
+                  {'games': my_games})
 
